07561c73461f0fbcefcd341be6de30482d67c6b3
Trozmagon <brett.bryant@outlook.com>
2024-08-15 15:49:03 +0100

    test

--- include/Functions.php
+++ include/Functions.php
@@ -1,9 +1,8 @@
 <?PHP
 ?>
 <?
-$modules = [];
-exec("sensors-detect --auto 2>&1|grep -Po \"^Driver.{2}\K[^\']*\"", $matches);
-foreach ($matches as $module) if (exec("modprobe -D $module 2>/dev/null")) $modules[] = $module;
-sort($modules);
-echo implode(' ',$modules);
+$devices = [];
+exec("bluetoothctl", $devices);
+sort($devices);
+echo implode(' ',$devices);
 ?>
